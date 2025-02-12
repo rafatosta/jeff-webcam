5c860e0fdb28c7c27f40a37a270e9cdce325f2fb
Rafael Tosta <rafa.ecomp@gmail.com>
2025-02-12 17:16:03 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useCallback, useEffect } from 'react';
+import { useRef, useState, useCallback, useEffect } from 'react';
 import Webcam from 'react-webcam';
 import { prominent } from 'color.js';
 import './App.css';
